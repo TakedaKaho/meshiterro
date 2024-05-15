1a747be06958e8aae9bbbdf4165bec38bbe4d8b6
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-15 09:29:16 +0000

    ユーザー情報追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
- 
-  get 'users/show'
-  get 'users/edit'
+
   devise_for :users #devise のモデルを作成したことによって勝手に追加された！
   root to: "homes#top"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   
   get 'homes/about' =>"homes#about", as: 'about'
   
