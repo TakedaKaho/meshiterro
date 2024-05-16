f6d8218f28fcb70841227bc4e2e815fda547d34b
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-16 10:19:39 +0000

    22章まで

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,12 +7,15 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params) #paramsとは、フォームなどによって送られてきた情報（パラメーター）を取得するメソッド
     @post_image.user_id = current_user.id #@post_image.user_id→　[モデル名].[カラム名]という形で繋げると、保存するカラムの中身を操作できる。
     #current_user は、コードに記述するだけで、ログイン中のユーザーの情報を取得できる便利な記述.devise をインストールすることで使える.
-    @post_image.save
+   if @post_image.save
     redirect_to post_images_path
+   else
+    render :new
+   end
   end 
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
